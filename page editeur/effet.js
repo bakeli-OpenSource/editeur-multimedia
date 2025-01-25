bedbed20afc32988994e16ab99fa7730d85071af
Baba Faye <fayebaba81@gmail.com>
2025-01-25 10:53:31 +0000

    ajout de filtre et telechargement apres modification

--- page editeur/effet.js
+++ page editeur/effet.js
@@ -1,34 +1,117 @@
-// recuperation des elements
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
 const fileInput = document.getElementById('lebutton');
-console.log(fileInput);
+const canvas = document.getElementById('canvas');
+const canvasContext = canvas.getContext('2d');
+const brightness = document.getElementById('brightness');
+const saturation = document.getElementById('saturation');
+console.log(saturation);
 
-const brightnessInput = document.getElementById('brightness');
+const blure = document.getElementById('blur');
+const inversion = document.getElementById('inversion');
+const contraste = document.getElementById('contraste');
+const noirEtBlanc = document.getElementById('noirblanc');
+const sapia = document.getElementById('sapia');
 
-let image;
-let setting = {}
-fileInput.addEventListener('change', () => {
-    const file = fileInput.files[0];
-    image.src = URL.createObjectURL(file);
-    const image = new Image();
-    image.onload = () => {
-        canvas.width = image.width;
-        canvas.height = image.height;
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+let image = null
+let isVideo = false
+// variable pour stocker les effets
+let setting = {
+    brightness: 100,saturation: 100,blur: 0,inversion: 0,contraste: 100,noirEtBlanc: 0,sapia: 0
+}
+
+
+// telecharger l'image depuis l'ordinateur
+fileInput.addEventListener("change",() => {
+    const file = fileInput.files[0]
+    const filleType = file.type
+
+    if(filleType.startsWith("image/")){
+        isVideo = false
+        image= new Image()
+        image.onload=() => {
+            initialiser()
+            rechargerImage()
+        }
+        // affichage de l'image en creant un canvas avec l'image
+        image.src = URL.createObjectURL(file)
+    }else if(filleType.startsWith("video/")){
+        isVideo = true
+        image= document.createElement('video')
+        image.controls = true
+        image.src = URL.createObjectURL(file)
+        image.onloadeddata = () => {
+            initialiser()
+            rechargerImage()
+        }
+    }else{
+        fileInput.value = ""
     }
-
+    
 })
 
-// function pour afficher l'image dans le canvas
-function displayImage(image) {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(image, 0, 0);
+// fonction initialisation
+function initialiser(){
+    brightness.value = setting.brightness
+    saturation.value = setting.saturation
+    blure.value = setting.blur
+    inversion.value = setting.inversion
+    contraste.value = setting.contraste
+    noirEtBlanc.value = setting.noirEtBlanc
+    sapia.value = setting.sapia
+}
+initialiser()
+// fonction pour charger l'image
+function rechargerImage(){
+    canvas.width = image.width || image.videoWidth
+    canvas.height = image.height || image.videoHeight
+    canvasContext.filter = generateFilter()
+
+    if(isVideo){
+    canvasContext.drawImage(image,0,0,canvas.width,canvas.height)
+    image.play()
+    drawVideoFrame()
+    }else{
+        canvasContext.drawImage(image,0,0,canvas.width,canvas.height)
+    }
 }
 
-// fonction pour les effets de l'image
-function appliquerEffet(){
- setting.brightness = brightnessInput= 100;
+// fonction pour dessiner chaque frame de la video
+function drawVideoFrame(){
+    if(!isVideo) return
+    canvasContext.filter = generateFilter()
+    canvasContext.drawImage(image,0,0,canvas.width,canvas.height)
+    requestAnimationFrame(drawVideoFrame)
 }
+// appliquer les effets
+generateFilter = () => {
+    const {
+        brightness,saturation,blur,inversion,contraste,noirEtBlanc,sapia} = setting
+    return `brightness(${brightness}%) saturate(${saturation}%) blur(${blur}px) invert(${inversion}%) contrast(${contraste}%) sepia(${sapia}%) grayscale(${noirEtBlanc}%)`
+}
+
+// fonction pour mettre a jour les effets
+function updateSetting(key,value){
+    if(!image) return
+        setting[key] = parseFloat(value)
+        rechargerImage()
+    }
+
+// appliquer les effets
+brightness.addEventListener('input',() => updateSetting('brightness',brightness.value))
+saturation.addEventListener('input',() => updateSetting('saturation',saturation.value))
+blure.addEventListener('input',() => updateSetting('blur',blur.value))
+inversion.addEventListener('input',() => updateSetting('inversion',inversion.value))
+contraste.addEventListener('input',() => updateSetting('contraste',contraste.value))
+noirEtBlanc.addEventListener('input',() => updateSetting('noirEtBlanc',noirEtBlanc.value))
+sapia.addEventListener('input',() => updateSetting('sapia',sapia.value))
+
+// telecharger l'image
+const downloadBtn = document.querySelector('.button-export')
+console.log(downloadBtn);
+
+function downloadMedia(){
+    const a = document.createElement('a')
+    a.href = canvas.toDataURL()
+    a.download = 'image.png'
+    a.click()
+}
+downloadBtn.addEventListener('click',downloadMedia)
