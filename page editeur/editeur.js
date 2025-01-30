d5903398b95045ab258005b1fbc5dc9a7bf94084
nafi badji <badjinafissatou943@gmail.com>
2025-01-30 16:13:11 +0000

    rognage

--- page editeur/editeur.js
+++ page editeur/editeur.js
@@ -1,7 +1,7 @@
-
-
 const affichageAll = document.querySelectorAll('.affichage')
 const allSide = document.querySelectorAll('.barre-effet .side')
+const textModif = document.querySelector('.aModif')
+
 
 //Boutton Filter
 const filter = document.getElementById('filtrer')
@@ -20,6 +20,8 @@ filter.addEventListener('click', ()=>{
     })
     filter.classList.add('active')
     afficherFilter.classList.add('active')
+    textModif.innerText = 'Filtre'
+    textModif.innerText = 'Filtre'
 })
 
 
@@ -40,6 +42,8 @@ duTexte.addEventListener('click', ()=>{
     })
     duTexte.classList.add('active')
     afficherText.classList.add('active')
+    textModif.innerText = 'Texte'
+    textModif.innerText = 'Texte'
 })
 
 
@@ -60,6 +64,8 @@ couper.addEventListener('click', ()=>{
     })
     couper.classList.add('active')
     afficherCoupure.classList.add('active')
+    textModif.innerText = 'Couper'
+    textModif.innerText = 'Couper'
 })
 
 
@@ -80,6 +86,8 @@ rogner.addEventListener('click', ()=>{
     })
     rogner.classList.add('active')
     afficherRogne.classList.add('active')
+    textModif.innerText = 'Redimensionner'
+    textModif.innerText = 'Redimensionner'
 })
 
 
@@ -100,5 +108,5 @@ dessin.addEventListener('click', ()=>{
     })
     dessin.classList.add('active')
     afficherDessin.classList.add('active')
+    textModif.innerText = 'Dessiner'
 })
-
