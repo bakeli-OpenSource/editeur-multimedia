98813892ab0c6df77b4708bfd5bbf68c5cb501f0
Baba Faye <fayebaba81@gmail.com>
2025-01-31 00:05:12 +0000

    suppression de doublon

--- page editeur/editeur.js
+++ page editeur/editeur.js
@@ -3,7 +3,6 @@ const allSide = document.querySelectorAll('.barre-effet .side')
 const textModif = document.querySelector('.aModif')
 
 
-const textModif = document.querySelector('.aModif')
 
 //Boutton Filter
 const filter = document.getElementById('filtrer')
